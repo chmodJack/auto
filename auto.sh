f52bc06f4622e1b7579b81b9da63d38eeab5e7b0
chmodJack <158754845@qq.com>
2018-05-25 16:38:28 +0800

    ok

---
+++ auto.sh
@@ -0,0 +1,4 @@
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh
+chmod a+x shadowsocksR.sh
+
+#./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
